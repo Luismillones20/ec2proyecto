ba7fc9f89034fe9eb5d402b35c5cab0f30e517bb
Luismillones20 <luis.millones@utec.edu.pe>
2025-04-19 17:21:22 -0500

    sintetizador aplicado

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from aws_cdk import App, Environment
+from aws_cdk import App, Environment, DefaultStackSynthesizer
 from ec2_proyecto.ec2_proyecto_stack import Ec2ProyectoStack
+
 
 app = App()
 
 # Configura el entorno con la cuenta y la región de AWS
 env = Environment(account="276665510567", region="us-east-1")
 
+
 # Inicializa el stack con el entorno configurado
-Ec2ProyectoStack(app, "Ec2ProyectoStack", env=env)
+Ec2ProyectoStack(app, "Ec2ProyectoStack", env=env,{
+  synthesizer: new DefaultStackSynthesizer({
+    qualifier: 'ec2proj',
+    cloudFormationExecutionRole = 'arn:aws:iam::276665510567:role/LabRole'
+  }),
+})
 
 app.synth()
--- ec2_proyecto/ec2_proyecto_stack.py
+++ ec2_proyecto/ec2_proyecto_stack.py
@@ -8,5 +8,4 @@ from constructs import Construct
 class Ec2ProyectoStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
-
         
