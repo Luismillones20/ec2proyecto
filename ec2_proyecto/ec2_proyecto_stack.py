43c2ea09f3bd170ebb25abade833d72ee4224783
Luismillones20 <luis.millones@utec.edu.pe>
2025-04-19 18:18:49 -0500

    cambios-stack

--- ec2_proyecto/ec2_proyecto_stack.py
+++ ec2_proyecto/ec2_proyecto_stack.py
@@ -17,16 +17,31 @@ class Ec2ProyectoStack(Stack):
             "us-east-1": "ami-0363234289a7b6202"  # Reemplaza con la ID real de la AMI de "Cloud9ubuntu22"
         })
 
-        # ARN del rol existente (LabRole)
-        role_arn = "arn:aws:iam::276665510567:role/LabRole"
+        # Crear un grupo de seguridad (Security Group)
+        security_group = ec2.SecurityGroup(self, "MySecurityGroup",
+            vpc=vpc,  # Asociarlo al VPC creado
+            security_group_name="MyInstanceSecurityGroup"
+        )
 
-        # Crear la instancia EC2 con el rol existente usando CfnInstance
+        # Abrir el puerto 22 para SSH
+        security_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(),  # Permitir acceso desde cualquier IP
+            ec2.Port.tcp(22),  # Puerto 22 (SSH)
+            "Allow SSH access from anywhere"
+        )
+
+        # Abrir el puerto 80 para HTTP
+        security_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(),  # Permitir acceso desde cualquier IP
+            ec2.Port.tcp(80),  # Puerto 80 (HTTP)
+            "Allow HTTP access from anywhere"
+        )
+
+        # Crear la instancia EC2 con el grupo de seguridad
         instance = ec2.CfnInstance(self, "MyInstance",
             instance_type="t2.micro",  # Ajusta el tipo de instancia según sea necesario
             image_id=ami.get_image(self).image_id,  # Obtén la ID de la imagen AMI
             subnet_id=vpc.public_subnets[0].subnet_id,  # Usando la primera subred pública del VPC
             key_name="vockey",  # Usamos 'vockey' como el par de claves existente
-            iam_instance_profile=role_arn,  # Asigna el ARN del rol existente
-            security_group_ids=[],  # Agrega grupos de seguridad si es necesario
+            security_group_ids=[security_group.security_group_id],  # Asignamos el grupo de seguridad creado
         )
-
