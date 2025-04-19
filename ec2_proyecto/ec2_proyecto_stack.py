dd3fc21a05bf4f63f603afc625eef7fa595d86c9
Luismillones20 <luis.millones@utec.edu.pe>
2025-04-19 17:48:46 -0500

    cambios en el proyecto

--- ec2_proyecto/ec2_proyecto_stack.py
+++ ec2_proyecto/ec2_proyecto_stack.py
@@ -2,7 +2,6 @@ from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
     aws_iam as iam,
-    aws_s3 as s3,
 )
 from constructs import Construct
 
@@ -10,15 +9,12 @@ class Ec2ProyectoStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        # Definir el par de claves (key pair) para la instancia EC2
-        key_name = "vockey"  # Asumiendo que ya has creado el par de claves `vockey` en la consola de AWS
-
         # Crear un VPC (Virtual Private Cloud) para la instancia EC2
         vpc = ec2.Vpc(self, "MyVpc", max_azs=3)  # Puedes cambiar el número de zonas de disponibilidad según lo necesites
 
-        # Definir la AMI de Cloud9Ubuntu22 (si está disponible en la región)
+        # Definir la AMI de Cloud9Ubuntu22 (reemplazar con la ID de la AMI correcta)
         ami = ec2.MachineImage.generic_linux({
-            "us-west-2": "ami-0363234289a7b6202"  # Reemplaza con la ID real de la AMI de "Cloud9ubuntu22"
+            "us-west-2": "ami-0363234289a7b6202"  
         })
 
         # Crear el rol de IAM para la instancia EC2 (puede ser una política básica para EC2)
@@ -32,7 +28,7 @@ class Ec2ProyectoStack(Stack):
             instance_type=ec2.InstanceType("t2.micro"),  # Ajusta el tipo de instancia según sea necesario
             machine_image=ami,
             vpc=vpc,
-            key_name=key_name,  # Asegúrate de que el par de claves `vockey` esté creado
+            key_pair=ec2.KeyPair.from_key_name(self, "KeyPair", "vockey"),  # Usa 'key_pair' en lugar de 'key_name'
             role=role,
             block_devices=[
                 ec2.BlockDevice(
@@ -51,4 +47,3 @@ class Ec2ProyectoStack(Stack):
             ec2.Port.tcp(80),  # Puerto 80 para HTTP
             "Allow HTTP access from anywhere"
         )
-
